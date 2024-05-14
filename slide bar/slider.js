e6e270f2a54a97c69bceaeae0766b62cfcf5d1e8
Ahmadreza0873 <ahmadrezaa0554@gmail.com>
2024-05-14 19:28:26 +0430

    1403/2/25

--- slide bar/slider.js
+++ slide bar/slider.js
@@ -1,17 +1,33 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+// var slideIndex = 1;
+// showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("slide");
+//   if (n > slides.length) {slideIndex = 1}    
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";  
+//   }
+//   slides[slideIndex-1].style.display = "block";  
+// }
+
+
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide() {
+  slides.forEach((slide) => {
+    slide.classList.remove('active');
+  });
+  slides[currentSlide].classList.add('active');
+
+  currentSlide = (currentSlide + 1) % slides.length;
 }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  slides[slideIndex-1].style.display = "block";  
-}
+showSlide();
+setInterval(showSlide, 5000);
